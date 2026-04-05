9d69e31a556617d429cd8024bd37ddd50365a162
Rubiee325 <jeyarubim@gmail.com>
2026-04-05 22:12:18 +0530

    fix build error

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
- baseURL: "http://localhost:5000/api",
+ baseURL: "https://financedashboard-enr7.onrender.com/api",
 });
 export const login = (data) => api.post('/auth/login', data);
 export const signup = (data) => api.post('/auth/signup', data);
